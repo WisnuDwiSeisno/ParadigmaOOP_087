5467bb5b81554e1f28bce8de37cfc9276aae82f6
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-05-24 20:27:36 +0700

    selesai membuat header ibu

--- mainIA/ibu.h
+++ mainIA/ibu.h
@@ -15,5 +15,14 @@ public:
 	void tambahAnak(anak*);
 	void cetakAnak();
 };
-
-#endif // !IBU_H
+void ibu::tambahAnak(anak* pAnak) {
+	daftar_anak.push_back(pAnak);
+}
+void ibu::cetakAnak() {
+	cout << "Daftar anak dari Ibu \"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+#endif 
