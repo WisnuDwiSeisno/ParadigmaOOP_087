a2f637b109a2b8a3fb92386b6caceb102fa4f43f
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-05-24 11:13:34 +0700

    selesai membuat constructor pelajar

--- ParadigmaOOP/ParadigmaOOP.cpp
+++ ParadigmaOOP/ParadigmaOOP.cpp
@@ -18,3 +18,16 @@ public:
 		return a + b;
 	}
 };
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+
+	~
+};
